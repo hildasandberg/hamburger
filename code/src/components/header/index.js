bb4bfe1b01b6651f8bf9820da6cdfe3e367c1cec
Hilda <hilda.sandberg@gmail.com>
2017-11-30 15:54:18 +0100

    okej

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -8,12 +8,31 @@ class Header extends React.Component {
   render() {
     return (
       <header>
+
+        <input id="hamburger" type="checkbox" className="hamburger-cb" />
+
+        <div className="headerRow">
+          <label className="hamburger" For="hamburger">
+            <div className="bar1" />
+            <div className="bar2" />
+            <div className="bar3" />
+          </label>
+        </div>
+
+        <ul className="menu">
+          <li><a href="http://technigo.io">Technigo</a></li>
+          <li><a href="http://technigo.io">Boot Camp</a></li>
+          <li><a href="http://technigo.io">Stories</a></li>
+          <li><a href="http://technigo.io">About</a></li>
+        </ul>
+
         <Grid className="header-grid">
           <a href="http://technigo.io">Technigo</a>
           <a href="http://technigo.io">Boot Camp</a>
           <a href="http://technigo.io">Stories</a>
           <a href="http://technigo.io">About</a>
         </Grid>
+
       </header>
     )
   }
